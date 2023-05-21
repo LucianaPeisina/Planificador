fd7c914f1bdc1b46b945b2d966a005b08b17d829
Hugo Jaca <hhjaca@gmail.com>
2023-05-21 16:50:27 -0300

    DER generado desde la BD Postgress y clave de la BD

--- Planificador/Planificador/settings.py
+++ Planificador/Planificador/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'planificador',
         'USER': 'postgres',
-        'PASSWORD': 'planificador',
+        'PASSWORD': 'sql',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
