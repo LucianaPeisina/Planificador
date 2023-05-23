00f44e13e14d3ee645ef30f6accedd1a308e5f56
Luciana Peisina <113636135+LucianaPeisina@users.noreply.github.com>
2023-05-23 06:58:59 -0300

    Actualizar models.py la parte de "Perfil" para que se cree apenas se crea el usuario

--- Planificador/planificador_comidas/models.py
+++ Planificador/planificador_comidas/models.py
@@ -3,20 +3,26 @@ from django.contrib.auth.models import User
 from django.db import models
 from datetime import datetime
 from django.urls import reverse
-
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Perfil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    cumpleanos = models.DateField(blank=True, null=True)
+    cumpleanos = models.DateField(null=True, blank=True)
     gustos = models.TextField(blank=True)
     disgustos = models.TextField(blank=True)
     extra = models.TextField(blank=True)
 
-    def __str__(self):
-        return self.user.username
     
-    
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Perfil.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.perfil.save()
 
 
 class Miembro(models.Model):
