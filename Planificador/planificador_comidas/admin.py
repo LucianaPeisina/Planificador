de51da5df51e40c01fae55132e22cb147fab84e0
Hugo Jaca <hhjaca@gmail.com>
2023-06-11 21:55:43 -0300

    Validaciones en front y back

--- Planificador/planificador_comidas/admin.py
+++ Planificador/planificador_comidas/admin.py
@@ -6,12 +6,10 @@ from .models import Perfil, Miembro
 # Registramos nuevos 'display' y 'filtros'
 @admin.register(Perfil)
 class PerfilAdmin(admin.ModelAdmin):
-    pass
     list_display = ('user', 'cumpleanos', 'gustos', 'extra')
     list_filter = ('gustos', 'extra')
     fields = [('user', 'cumpleanos'), 'gustos', 'extra']
 
 @admin.register(Miembro)
 class MiembroAdmin(admin.ModelAdmin):
-    pass
     list_display = ('usuario', 'nombre', 'edad')
